b59fad07dfc2c3819618beb64419a499926b8275
Vilém Raška <v.rashka.ext@make.com>
2025-11-06 14:49:54 +0100

    feat: update aliases

--- aliases/common.sh
+++ aliases/common.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-alias ls='ls -la'
-alias sl='ls -la'
-alias l='ls -la'
-alias ll='ls -la'
+# ls aliases
+alias llM='ls -al --block-size=M'
+alias llk='ls -al --block-size=k'
+alias ll='ls -al'
+alias la='ls -A'
+alias l='ls -CF'
+alias sl='ls'
 
 alias cl='clear'
--- aliases/docker.sh
+++ aliases/docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+alias docker-compose='docker compose'
+alias dc='docker compose'
+alias dcu='docker compose up -d'
 alias dcd='docker compose down'
-alias dcu='docker compose up'
